61c7e8e00de79512d03889d4b530c176c0806ac3
Pavel Botos <Pavel.Botos@konicaminolta.cz>
2018-08-18 11:29:08 +0200

    Added DecelerationEngine tests and some amount of functionality

--- src/MoonLanderAutopilot1D.h
+++ src/MoonLanderAutopilot1D.h
@@ -10,14 +10,30 @@ namespace Botos_Example {
 
     class AccelerationService {
     public:
-        static float acceleration(float force, float weight) {
-            return force/weight;
+        float acceleration(float force, float weight) {
+            return force / weight;
         }
     };
 
     class PropellantTank {
+    private:
         // weight is in kgs
         float propellantWeight = 10000.0f;
+    public:
+        void setPropellantWeight(float propellantWeight) {
+            this->propellantWeight = propellantWeight;
+        };
+
+        virtual float getPropellantWeight() {
+            return propellantWeight;
+        };
+
+        PropellantTank() {};
+
+        ~PropellantTank() {
+
+        };
+
     };
 
     class DescentEngine {
@@ -25,31 +41,72 @@ namespace Botos_Example {
         // in Newtons
         float thrust = 44316.0f;
 
-        // fuel consuption in kg per second
-        float consuption = 28.0f;
+        // fuel consumption in kg per second
+        float consumption = 28.0f;
+
+        PropellantTank *propellantTank;
+
+        bool running;
+
 
     public:
-        /**
-         * @param time
-         * @return consumed fuel in kgs
-         */
-        virtual float ignite(float time);
-        virtual ~DescentEngine();
+        DescentEngine(PropellantTank *propellantTank, float thrust, float consumption) {
+            this->propellantTank = propellantTank;
+            this->thrust = thrust;
+            this->consumption = consumption;
+            this->running = false;
+        }
+
+        void ignite() {
+            running = true;
+        }
+
+        void stop() {
+            running = false;
+        }
+
+        ~DescentEngine() {
+            stop();
+            delete this->propellantTank;
+        };
+
+        float getThrust(float timeDelta){
+            if(running) {
+                propellantTank->setPropellantWeight(propellantTank->getPropellantWeight() - (timeDelta * consumption));
+                return thrust;
+            }
+            return 0.0f;
+        }
     };
 
+
+    class MoonLanderAutopilot1D {
+        DescentEngine *descentEngine;
+
+    };
 
     class LunarModule {
     private:
         // weight is in kgs
         float moduleWeight = 4000.0f;
+        float actualHeight = 0.0f;
+        float actualSpeed = 0.0f;
+        PropellantTank *propellantTank;
+        DescentEngine *descentEngine;
+
+    public:
+        LunarModule(DescentEngine *descentEngine, PropellantTank *propellantTank, float initialHeight){
+            this->descentEngine = descentEngine;
+            this->propellantTank = propellantTank;
+            this->actualHeight = initialHeight;
+        }
+
+        ~LunarModule();
 
 
     };
 
 
-    class MoonLanderAutopilot1D {
-
-    };
 
 }
 
--- test/MoonLanderAutopilot1DUnitTest.cpp
+++ test/MoonLanderAutopilot1DUnitTest.cpp
@@ -36,16 +36,18 @@ BOOST_AUTO_TEST_CASE(moonlander_autopilot_1d_test_acceleration_service_1) {
     /**
      * Given
      */
-     // force in Newtons
+    // force in Newtons
     float force = 10;
 
     // weight in kgs
     float weight = 10;
 
+    AccelerationService *accelerationService = new AccelerationService();
+
     /**
      * When
      */
-    float acceleration = AccelerationService::acceleration(force, weight);
+    float acceleration = accelerationService->acceleration(force, weight);
 
     /**
      * Then
@@ -65,15 +67,108 @@ BOOST_AUTO_TEST_CASE(moonlander_autopilot_1d_test_acceleration_service_2) {
     // weight in kgs
     float weight = 14000.0f;
 
+    AccelerationService *accelerationService = new AccelerationService();
+
     /**
      * When
      */
-    float acceleration = AccelerationService::acceleration(force, weight);
+    float acceleration = accelerationService->acceleration(force, weight);
 
     /**
      * Then
      */
     BOOST_CHECK_EQUAL(3.16542864f, acceleration);
 
+
+}
+
+BOOST_AUTO_TEST_CASE(descending_engine_off_test_consumption) {
+    /**
+     * Given
+     */
+
+    PropellantTank *propellantTank = new PropellantTank();
+    propellantTank->setPropellantWeight(20000.0f);
+    DescentEngine *descentEngine = new DescentEngine(propellantTank, 10000.0f, 30.0f);
+
+    /**
+     * When engine is turned off it is giving no thrust and cosumes no fuel.
+     */
+    float thrust = descentEngine->getThrust(1.0);
+
+    /**
+     * Then
+     */
+    BOOST_CHECK_EQUAL(0.0, thrust);
+
+    BOOST_CHECK_EQUAL(20000.0f, propellantTank->getPropellantWeight());
+
+}
+
+BOOST_AUTO_TEST_CASE(descending_engine_on_test_consumption) {
+    /**
+     * Given
+     */
+
+    PropellantTank *propellantTank = new PropellantTank();
+    propellantTank->setPropellantWeight(20000.0f);
+    DescentEngine *descentEngine = new DescentEngine(propellantTank, 10000.0f, 30.0f);
+
+    /**
+     * When engine is turned off it is giving no thrust and cosumes no fuel.
+     */
+    descentEngine->ignite();
+    float thrust = descentEngine->getThrust(10.0);
+
+    /**
+     * Then
+     */
+    BOOST_CHECK_EQUAL(10000.0, thrust);
+
+    BOOST_CHECK_EQUAL(19700.0f, propellantTank->getPropellantWeight());
+
+}
+
+BOOST_AUTO_TEST_CASE(moonlander_autopilot_1d_test_decelleration_1) {
+    /**
+     * Given
+     */
+    // force in Newtons
+    float force = 44316.0f;
+
+    // weight in kgs
+    float lmWeight = 4000.0f;
+    float fuelWeight = 10000.0f;
+    float weight = lmWeight + fuelWeight;
+    // in meter per second
+    float initialDescentSpeed = 68.8f;
+
+    // in meter per second square
+    float moonGravity = 1.62f;
+    float time = 0.0f;
+
+    // in meters
+    float heightAboveMoon = 15000.0f;
+
+
+    /**
+     * When
+     */
+    float speed = initialDescentSpeed;
+    while (heightAboveMoon > 0.0f) {
+        float timeFraction = 0.1f;
+        time += timeFraction;
+        speed = initialDescentSpeed + (moonGravity * time);
+        heightAboveMoon -= speed * timeFraction;
+        std::cout << "time: " << time << " speed: " << speed << " height: " << heightAboveMoon << std::endl;
+    }
+
+    /**
+     * Then
+     */
+    BOOST_CHECK_LE(speed, 1.0f);
+    BOOST_CHECK_GE(speed, 0.0f);
+    BOOST_CHECK_EQUAL(0.0f, time);
+    BOOST_CHECK_LE(heightAboveMoon, 0.0f);
 
 }
